1ceef8039cf7dd9560f62284ff1a87c3846c1479
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-05-28 08:22:57 +0200

    Add files via upload

---
+++ TrådSynkroniseringØvelse1/TrådSynkroniseringØvelse1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TrådSynkroniseringØvelse1
+{
+    class Program
+    {
+        static object _lock = new object();
+        static int threadCount = 0;
+
+        static void Main(string[] args)
+        {
+            Thread countUp = new Thread(new ThreadStart(CountTwoUp));
+            Thread countDown = new Thread(new ThreadStart(CountOneDown));
+
+            countUp.Start();
+            countDown.Start();
+            Console.Read();
+        }
+
+        public static void CountTwoUp()
+        {
+            while (true)
+            {
+
+                Monitor.Enter(_lock);
+                threadCount++;
+                threadCount++;
+                Console.WriteLine(threadCount);
+                Thread.Sleep(1000);
+                Monitor.Exit(_lock);
+            }
+        }
+
+        public static void CountOneDown()
+        {
+            while (true)
+            {
+
+                Monitor.Enter(_lock);
+                threadCount--;
+                Console.WriteLine(threadCount);
+                Thread.Sleep(1000);
+                Monitor.Exit(_lock);
+            }
+        }
+    }
+}
---
+++ TrådSynkroniseringØvelse2/TrådSynkroniseringØvelse2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TrådSynkroniseringØvelse2
+{
+    class Program
+    {
+        public static object _lock = new object();
+        public static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            Thread writeStars = new Thread(new ThreadStart(OutputStars));
+            Thread writeHashtags = new Thread(new ThreadStart(OutputHashtags));
+
+            writeStars.Start();
+            writeHashtags.Start();
+
+            Console.Read();
+        }
+
+        public static void OutputStars()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("*");
+                        counter++;
+                    }
+                    Console.WriteLine(" Count: " + counter);
+                    Thread.Sleep(1000);
+
+                }
+                catch (Exception e)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+
+        public static void OutputHashtags()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+
+                        Console.Write("#");
+                        counter++;
+                    }
+                    Console.WriteLine(" Count: " + counter);
+                    Thread.Sleep(1000);
+                }
+
+                catch (Exception e)
+                {
+
+                    throw;
+                }
+
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+    }
+}
